1686227059ca93059d333cc865fc92c578fa740f
Emmaleiva95 <emmaleiva95@gmail.com>
2024-11-12 02:33:35 +0100

    usar variables de entorno y gitignore

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mockRoutes from './src/routes/mock.router.js'
 import { connectDB } from './db.js'
 import {__dirname} from './utils.js';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 
@@ -9,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/mocks',mockRoutes)
+app.use('/api/mocks', mockRoutes)
 
 const httpServer = app.listen(8080, () => console.log('Servidor listo en el puerto '+ 8080));
 connectDB();
--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://manutesting:manu1234@clustertestingdb.yqbmfuc.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTestingDB');
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('DB CONNECTED')
     } catch (error) {
         console.log(error)
